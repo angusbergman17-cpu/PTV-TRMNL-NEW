b52b55eace87df7b41b1524ace018f4125985f38
Angus Bergman <angusbergman@Anguss-MacBook-Pro.local>
2026-01-28 12:27:49 +1100

    security: Remove hardcoded API keys from firmware config.h

--- firmware/include/config.h
+++ firmware/include/config.h
@@ -6,14 +6,13 @@
  * Generic template - device fetches personalized config from server
  *
  * Copyright (c) 2026 Angus Bergman
- * Licensed under CC BY-NC 4.0 (Creative Commons Attribution-NonCommercial 4.0 International License)
- * https://creativecommons.org/licenses/by-nc/4.0/
+ * Licensed under CC BY-NC 4.0
  */
 
 // BYOS Server Configuration
 // Device will fetch personalized config token from setup wizard
 #define SERVER_URL "https://ptvtrmnl.vercel.app"
-#define API_DISPLAY_ENDPOINT "/api/device/eyJhIjp7ImhvbWUiOiIxIENsYXJhIFN0cmVldCwgU291dGggWWFycmEgVklDIDMxNDEiLCJjYWZlIjoiTm9ybWFuIFNvdXRoIFlhcnJhIiwiY2FmZU5hbWUiOiJOb3JtYW4iLCJ3b3JrIjoiODAgQ29sbGlucyBTdHJlZXQsIE1lbGJvdXJuZSBWSUMgMzAwMCJ9LCJqIjp7Im51bWJlck9mTW9kZXMiOjIsIm1vZGUxIjp7InR5cGUiOjEsIm9yaWdpblN0YXRpb24iOnsibmFtZSI6IlRvb3JhayBSZC9DaGFwZWwgU3QiLCJpZCI6IjI4MDMiLCJsYXQiOi0zNy44NCwibG9uIjoxNDQuOTk4fX0sIm1vZGUyIjp7InR5cGUiOjAsIm9yaWdpblN0YXRpb24iOnsibmFtZSI6IlNvdXRoIFlhcnJhIiwiaWQiOiIxMTU5IiwibGF0IjotMzcuODM4NSwibG9uIjoxNDQuOTkyOX0sImRlc3RpbmF0aW9uU3RhdGlvbiI6eyJuYW1lIjoiUGFybGlhbWVudCIsImlkIjoiMTEyMCIsImxhdCI6LTM3LjgxMSwibG9uIjoxNDQuOTczfX19LCJsIjp7fSwicyI6IlZJQyIsImsiOiJjZTYwNmI5MC05ZmZiLTQzZTgtYmNkNy0wYzJiZDA0OTgzNjciLCJnIjoiQUl6YVN5QTlXWXBSZkx0QmlFUWZ2VEQtYWM0SW1IQm9oSHN2M3lRIn0"  // Generic endpoint, device fetches config
+#define API_DISPLAY_ENDPOINT "/api/device/"  // Append device token here
 #define API_SETUP_ENDPOINT "/api/setup"
 #define API_LOG_ENDPOINT "/api/log"
 #define API_DEVICE_CONFIG_ENDPOINT "/api/device-config"
@@ -48,28 +47,24 @@
 #define TIME_X 20
 #define TIME_Y 10
 #define TIME_W 135
-#define TIME_H 50
+#define TIME_H 48
 
-#define TRAIN_X 15
-#define TRAIN_Y 105
-#define TRAIN_W 200
-#define TRAIN_H 60
+#define DEPARTURES_X 30
+#define DEPARTURES_Y 100
+#define DEPARTURES_W 350
+#define DEPARTURES_H 300
 
-#define TRAM_X 15
-#define TRAM_Y 215
-#define TRAM_W 200
-#define TRAM_H 60
+#define JOURNEY_BOX_X 500
+#define JOURNEY_BOX_Y 110
+#define JOURNEY_BOX_W 280
+#define JOURNEY_BOX_H 140
 
-#define COFFEE_X 480
-#define COFFEE_Y 10
-#define COFFEE_W 310
-#define COFFEE_H 30
+// NTP Configuration
+#define NTP_SERVER "pool.ntp.org"
+#define NTP_OFFSET_SECONDS 39600  // UTC+11 (AEDT)
+#define NTP_UPDATE_INTERVAL 60000
 
-#define BATTERY_PIN 1
-#define LOW_BATTERY_MV 3300
-#define SLEEP_BETWEEN_PARTIALS_MS 18000
-#define MIN_FREE_HEAP 100000
-#define MAX_PNG_SIZE 81920
-#define JSON_BUFFER_SIZE 4096
+// Firmware Version
+#define FIRMWARE_VERSION "v5.18"
 
 #endif // CONFIG_H
